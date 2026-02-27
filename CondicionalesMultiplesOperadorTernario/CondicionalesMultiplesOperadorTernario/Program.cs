0480e4e41ebec728aec4154ed394d31528a4b901
3asedSyms <agus.b1215@protonmail.com>
2026-02-27 13:27:10 -0500

    Hasta condicionalesMultiples, falta calculadora

---
+++ CondicionalesMultiplesOperadorTernario/CondicionalesMultiplesOperadorTernario/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+
+namespace CondicionalesMultiplesOperadorTernario
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* string diaSemana = "";
+            float subtotal = 0;
+            float descuento = 0;
+
+            Console.WriteLine("Ingrese monto de la compra");
+            subtotal = Single.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el dia de la semana");
+            diaSemana = Console.ReadLine().ToLower();
+
+            switch (diaSemana)
+            {
+                case "lunes":
+                    descuento = subtotal * 0.1f;
+                    Console.WriteLine($"Hoy es lunes, el descuento es de {descuento}, el valor total a pagar es de {subtotal - descuento}");
+                    break;
+                case "martes":
+                    descuento = subtotal * 0.15f;
+                    Console.WriteLine($"Hoy es martes, el descuento es de {descuento}, el valor total a pagar es de {subtotal - descuento}");
+                    break;
+                case "miercoles":
+                    descuento = subtotal * 0.1f;
+                    Console.WriteLine($"Hoy es miercoles, el descuento es de {descuento}, el valor total a pagar es de {subtotal - descuento}");
+                    break;
+                case "jueves":
+                    descuento = subtotal * 0.15f;
+                    Console.WriteLine($"Hoy es jueves, el descuento es de {descuento}, el valor total a pagar es de {subtotal - descuento}");
+                    break;
+                case "viernes":
+                    descuento = subtotal * 0.2f;
+                    Console.WriteLine($"Hoy es viernes, el descuento es de {descuento}, el valor total a pagar es de {subtotal - descuento}");
+                    break;
+                case "sabado":
+                    descuento = subtotal * 0.2f;
+                    Console.WriteLine($"Hoy es sabado, el descuento es de {descuento}, el valor total a pagar es de {subtotal - descuento}");
+                    break;
+                case "domingo":
+                    descuento = 0;
+                    Console.WriteLine($"Hoy es domingo, el descuento es de {descuento}, el valor total a pagar es de {subtotal - descuento}");
+                    break;
+                
+                default:
+                    Console.WriteLine("Porfavor ingrese un dia de la semana correcto");
+                    break;
+            }*/
+
+            float num1 = 0f;
+            float num2 = 0f;
+            float suma = num1 + num2;
+            float resta = num1 - num2;
+            float multiplicacion = 3f;
+            float division = 4f;
+
+            Console.WriteLine("Ingrese operacion ");
+            suma = Single.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese numero 1");
+            
+
+        }
+    }
+}
