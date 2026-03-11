fbf74d57a591e23805d24d9aaf582410d0b7caf9
3asedSyms <agus.b1215@protonmail.com>
2026-03-11 13:30:39 -0500

    Ejercicio9

---
+++ 9.Acumulador_y_Contador/9.Acumulador_y_Contador/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+
+namespace _9.Acumulador_y_Contador
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // generar los primeros cinco numeros y sumarlos hasta llegar a 15
+            /*
+                        int contador = 1;
+                        int acumulador = 0;
+
+                        while (contador <= 5)
+                        {
+                            acumulador += contador;
+                            Console.WriteLine($"
+                            contador++;
+
+                        }
+                        Console.WriteLine($"La suma de los cinco números enteros es:{acumulador}");
+                 */
+
+
+            int numero = 0;
+            int contador = 1;
+            int acumulador = 1;
+            int newNum = 0;
+
+            Console.WriteLine("Ingrese un numero");
+
+            numero = int.Parse(Console.ReadLine());
+
+            while (contador <= numero)
+            {
+                acumulador = contador * numero;
+                contador ++;                      
+                newNum = contador * acumulador;
+            }
+            
+            Console.WriteLine(newNum);
+        }
+    }
+}
---
+++ 9.Acumulador_y_Contador/9.Acumulador_y_Contador/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("9.Acumulador_y_Contador")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("9.Acumulador_y_Contador")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("1b08eaaa-c56a-4172-a9bb-d40776c34d76")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
