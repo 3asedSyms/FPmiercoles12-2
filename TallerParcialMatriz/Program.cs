c6d48f60e827bf40c85926b2899a56f587137d9f
B09S104est <B09S104est@MEDAULAS.UPB>
2026-04-22 13:17:42 -0500

    InicioTallerParcial-M

---
+++ TallerParcialMatriz/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TallerParcialMatriz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Desarrollar un programa que crea una matriz de 10 filas y 20 columnas y muestre por
+            pantalla la suma de los elementos de cada columna.*/
+            
+            int[,] MatrizA = [10, 20];
+
+
+
+        }
+    }
+           
+}
